10102e27fc26b546cffe97205655902dfc290d64
Hrishikesh Kolhe <hrishikeshkolhe24@gmail.com>
2024-04-26 23:27:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherApp from "./components/weatherApp/WeatherApp";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
